8f141d2c67b65f8927ba9161f25f18fcee650727
DeD MorozZz <morozzz.g2@gmail.com>
2016-03-14 16:22:09 +0300

    Empty commit

--- Config.hpp
+++ Config.hpp
@@ -1,8 +1,8 @@
 #ifndef CONFIG_HPP_INCLUDED
 #define CONFIG_HPP_INCLUDED
 
-#define COM_DEFAULT_PORT "\\\\.\\COM5"
-#define COM_DEFAULT_BRATE 115200
+#define COM_DEFAULT_PORT "\\\\.\\COM9"
+#define COM_DEFAULT_BRATE 9600
 #define COM_DEFAULT_DATA 8
 #define COM_DEFAULT_STOP SerialPortStop::Two
 #define COM_DEFAULT_PARITY SerialPortParity::Mark
